2d81293702aa1185d07f317c4195198afec2b7a4
CoffeeYes <culinecro@gmail.com>
2020-11-20 14:49:14 +0100

    remove old testing and example functions

--- android/src/main/java/com/reactnativecrypto/CryptoModule.kt
+++ android/src/main/java/com/reactnativecrypto/CryptoModule.kt
@@ -73,34 +73,6 @@ class CryptoModule(reactContext: ReactApplicationContext) : ReactContextBaseJava
         return constants
     }
 
-
-    // Example method
-    // See https://facebook.github.io/react-native/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-
-      promise.resolve(a * b)
-
-    }
-
-    @ReactMethod
-    fun test(promise : Promise) {
-      val cipher : Cipher = Cipher.getInstance("AES");
-      val encryptionKeyString : String =  "thisisa128bitkey";
-	    val originalMessage : String = "This is a secret message";
-	    val encryptionKeyBytes : ByteArray = encryptionKeyString.toByteArray();
-
-      val secretKey = SecretKeySpec(encryptionKeyBytes,"AES");
-
-      cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-      val encryptedMessageBytes : ByteArray = cipher.doFinal(originalMessage.toByteArray());
-
-      cipher.init(Cipher.DECRYPT_MODE, secretKey);
-	    val decryptedMessageBytes : ByteArray = cipher.doFinal(encryptedMessageBytes);
-
-      promise.resolve(String(decryptedMessageBytes));
-    }
-
     @ReactMethod
     fun generateRSAKeyPair(alias : String, promise : Promise) {
 
